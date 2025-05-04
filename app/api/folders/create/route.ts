c2d61fa3f440cb6ec0240880a40a5a27f4193af0
imran-techvoyager <imran.techvoyager@gmail.com>
2025-05-04 08:08:06 +0530

    Folder structuring

--- app/api/folders/create/route.ts
+++ app/api/folders/create/route.ts
@@ -1,7 +1,6 @@
 import { db } from "@/lib/db";
 import { files } from "@/lib/db/schema";
 import { auth } from "@clerk/nextjs/server";
-import { error } from "console";
 import { eq, and } from "drizzle-orm"
 import { NextRequest, NextResponse } from "next/server";
 import {v4 as uuidv4} from "uuid"
